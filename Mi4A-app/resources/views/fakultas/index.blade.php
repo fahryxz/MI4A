1f07a3efd9bf85608681f1fe31083a0932a6d2b9
Fahry <fahrywolf88@mhs.mdp.ac.id>
2023-04-29 15:10:33 +0700

    laravel

---
+++ Mi4A-app/resources/views/fakultas/index.blade.php
@@ -0,0 +1,7 @@
+<h2>Fakultas</h2>
+ini adalah halaman fakultas
+<ul>
+    @foreach ($fakultas as $item)
+        <li>{{ $item }}</li>
+    @endforeach
+</ul>
--- Mi4A-app/routes/web.php
+++ Mi4A-app/routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/halo', function () {
+    return "Halo Semua";
+});
+
+Route::get('/mahasiswa/{nama}', function ($nama) {
+    echo "<h2>Nama saya $nama</h2>";
+});
+
+Route::get('/profil/{nama?}/{pekerjaan?}', function ($nama = "Fahry", $pekerjaan = "Mahasiswa"){
+    echo "<h2>Nama saya $nama, pekerjaan $pekerjaan";
+});
+
+Route::get('/dosen', function (){
+    return view('dosen');
+});
+
+Route::get('/jadwal', function () {
+    return view('jadwal.index');
+});
+
+Route::get('/fakultas', function (){
+    return view('fakultas.index')->with('fakultas',["FIKR", "FEB"]);
+});
